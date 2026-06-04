3809cf9543ec43b11f766d8b00ecb1e3101c7004
Pedrozzq <pdrao13@gmail.com>
2026-06-04 12:39:13 -0300

    Update create-payment.js

--- api/create-payment.js
+++ api/create-payment.js
@@ -1,7 +1,7 @@
 const { MercadoPagoConfig, Preference } = require("mercadopago");
 
 module.exports = async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://www.jpjoiasbrasil.com");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
