32fe4f8a20b64dc80827b4784a43bb7330554fe4
Pedrozzq <pdrao13@gmail.com>
2026-06-04 01:21:38 -0300

    Update create-payment.js

--- api/create-payment.js
+++ api/create-payment.js
@@ -1,1 +1,44 @@
+const { MercadoPagoConfig, Preference } = require("mercadopago");
 
+module.exports = async (req, res) => {
+
+  if (req.method !== "POST") {
+    return res.status(405).json({
+      error: "Método não permitido"
+    });
+  }
+
+  try {
+
+    const client = new MercadoPagoConfig({
+      accessToken: process.env.MERCADOPAGO_ACCESS_TOKEN
+    });
+
+    const preference = new Preference(client);
+
+    const result = await preference.create({
+      body: {
+        items: [
+          {
+            title: "Produto Teste JP Joias",
+            quantity: 1,
+            currency_id: "BRL",
+            unit_price: 1
+          }
+        ]
+      }
+    });
+
+    return res.status(200).json({
+      init_point: result.init_point
+    });
+
+  } catch (error) {
+
+    return res.status(500).json({
+      error: error.message
+    });
+
+  }
+
+};
